9a6b303d96d5ce3229b32152043d1e6b1f426790
Dakota Ramey <71187507+Dakotaramey6@users.noreply.github.com>
2023-12-20 00:42:44 -0500

    Created workout calendar base

---
+++ WorkoutTab.js
@@ -0,0 +1,83 @@
+let dateMonth = document.getElementById("date-month");
+let dateYear = document.getElementById("date-year");
+let tableBody = document.getElementById("table-body");
+let currentDate = new Date();
+const currentMonth = currentDate.getUTCMonth();
+const currentYear = currentDate.getUTCFullYear();
+let daysOfMonth = [];
+
+let maxYear = 2030;
+
+const years = [];
+
+let oldYear = currentYear - 2;
+while (maxYear >= oldYear) {
+  years.push(oldYear);
+  oldYear++;
+}
+
+function createOptions(dateType, arr, docEl, compare) {
+  options = document.createElement("option");
+  options.text = dateType;
+  if (typeof dateType === "string") {
+    if (arr.indexOf(dateType) === compare) options.selected = "selected";
+  } else {
+    if (dateType === compare) options.selected = "selected";
+  }
+  docEl.appendChild(options);
+}
+
+const months = [
+  "January",
+  "Febuary",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+months.forEach((month) => {
+  createOptions(month, months, dateMonth, currentMonth);
+});
+
+years.forEach((year) => {
+  createOptions(year, years, dateYear, currentYear);
+});
+
+function cleanCalenderDays() {
+  daysOfMonth = [];
+  tableBody.replaceChildren(...daysOfMonth);
+}
+
+function createDaysForCalenderArr(arr, num) {
+  i = 1;
+  while (i <= num) {
+    arr.push(i);
+    i++;
+  }
+}
+
+function changeMonth() {
+  cleanCalenderDays();
+  if (dateMonth.value === "Febuary") {
+    createDaysForCalenderArr(daysOfMonth, 28);
+  } else if (dateMonth === "April" || dateMonth === "June") {
+    createDaysForCalenderArr(daysOfMonth, 30);
+  } else {
+    createDaysForCalenderArr(daysOfMonth, 31);
+  }
+  for (let i = 1; i <= daysOfMonth.length; i++) {
+    let days = document.createElement("td");
+    days.innerText = i;
+    tableBody.appendChild(days);
+  }
+  console.log(dateMonth.value);
+}
+
+dateMonth.onchange = changeMonth;
